f3c46594f7d4fde803ce3c15e24aa67fa6339531
Tim Ward <tsy23@wildcats.unh.edu>
2019-10-27 18:34:41 -0400

    update index.php

--- index.php
+++ index.php
@@ -17,31 +17,17 @@
         -->
         <br><br>
         <?php
-            #echo "CRYING BONERS";
-            //TODO: get this working
             $dir    = 'seg_files';
             $video_files = [];
             foreach(glob($dir.'/*') as $file) {
-                #echo "WHINING BONERS: $file";
                 foreach(glob($file.'/*') as $seg_file) {
-                    #echo "SQUEALING BONERS: $seg_file";
                     $file_parts = pathinfo($seg_file);
                     if ($file_parts['extension'] == "m3u8"){
-                        echo "BUMPING BONERS $seg_file";
+                        echo "seg_file $seg_file";
                         echo "<br>";
-                        //echo "wowee";
-                        
-                        $frump = array_pop(explode('/', $seg_file));
-                        
-                        //$tmp = substr($file, strpos($file, "/") + 1);
-                        //$tmp = substr($tmp, strpos($file, "/") + 1);
-                        #$tmp = substr($file, strpos($file, "/") + 1);
-                        echo "THUMPING BONERS $frump";
-                        echo "<br>";    
-                        //print $seg_file;
-                        //echo "<br>";
-                        if($frump == "master.m3u8") {
-                            echo("MENSTRUATING JABRONIES<br>");
+                        $ext = array_pop(explode('/', $seg_file));
+                        if($ext == "master.m3u8") {
+                            echo("ext $ext<br>");
                             echo "<video controls>";
                             echo "<source src=". $seg_file ." type='video/mp4'>";
                             echo "</video><br/>";
@@ -50,7 +36,6 @@
                     }
                 }
             }
-            //
         ?>
         <br><br><br><br>
         <br><br><br><br>
